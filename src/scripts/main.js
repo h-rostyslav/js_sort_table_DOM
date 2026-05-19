375b2b5b47b9ddacd07d91ad79fc6b0d223a033c
Rostyslav` <hrostyslav@ukr.net`>
2026-05-19 12:19:55 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+table.addEventListener('click', (e) => {
+  const tbody = document.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+  const th = e.target.closest('th');
+  if(!th) return;
+  const columnIndex = th.cellIndex;
+
+
+  rows.sort((a, b) => {
+    const valueA = a.cells[columnIndex].textContent;
+    const valueB = b.cells[columnIndex].textContent;
+
+    return valueA.localeCompare(valueB);
+  });
+  
+  rows.forEach(row => tbody.appendChild(row));
+});
